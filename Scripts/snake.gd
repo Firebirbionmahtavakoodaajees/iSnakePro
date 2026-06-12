42b468e4cd86cc268bf50342af234905c1702a8d
Ruiyan Tang <kivasopo@outlook.com>
2026-06-12 12:34:44 +0300

    Update snake.gd
    Updated the known bug

--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @onready var timer:Timer = $Timer #:BTW the :Timer just tells var type
 const SnakeSize = 225
-var direction = 1
+var Direction = 1
+var PrevDirection = 3
 
 # Centers the snake box around the snake node's origin
 @onready var snake_rect = Rect2(global_position - Vector2(SnakeSize, SnakeSize) / 2, Vector2(SnakeSize, SnakeSize))
@@ -17,29 +18,36 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Right"):
-		if direction != 3:
-			direction = 1
+		if PrevDirection != 3:
+			Direction = 1
 	elif Input.is_action_just_pressed("Down"):
-		if direction != 4:
-			direction = 2
+		if PrevDirection != 4:
+			Direction = 2
 	elif Input.is_action_just_pressed("Left"):
-		if direction != 1:
-			direction = 3
+		if PrevDirection != 1:
+			Direction = 3
 	elif Input.is_action_just_pressed("Up"):
-		if direction != 2:
-			direction = 4
-	
+		if PrevDirection != 2:
+			Direction = 4
+			
 	pass
 
 func MoveTick() -> void:
-	if direction == 1:
+
+	if Direction == 1:
+		PrevDirection == 1
 		position.x += -SnakeSize
-	elif direction == 2:
+	elif Direction == 2:
+		PrevDirection == 2
 		position.y += -SnakeSize
-	elif direction == 3:
+	elif Direction == 3:
+		PrevDirection == 3
 		position.x += SnakeSize
 	else:
+		PrevDirection == 4
 		position.y +=SnakeSize
+		
+	PrevDirection = Direction
 	
 	snake_rect.position = global_position
 	
